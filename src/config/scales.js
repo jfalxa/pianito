0a9739e342ab4552c500ff974e535c19972376e5
jfalxa <jfalxa.sta@gmail.com>
2019-12-14 17:14:19 +0100

    project setup

--- src/config/scales.js
+++
@@ -1,7 +0,0 @@
-import { steps } from '../utils/helpers'
-
-export const MAJOR = [1, 1, 1 / 2, 1, 1, 1, 1 / 2]
-export const MINOR = [1, 1 / 2, 1, 1, 1 / 2, 1, 1]
-
-export const MAJOR_STEPS = steps(MAJOR)
-export const MINOR_STEPS = steps(MINOR)
--- src/hooks/inputs/index.js
+++
@@ -1,9 +0,0 @@
-import useMIDI from './midi'
-import useMouse from './mouse'
-import useKeyboard from './keyboard'
-
-export default {
-  useMIDI,
-  useMouse,
-  useKeyboard
-}
