f3ebd0608ee29b34400d7f40babc83cd7cc77eb3
jfalxa <jfalxa.sta@gmail.com>
2020-11-23 12:34:40 +0100

    app can show and play a given track

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+import Synth from "./synth.js";
+
 const cl = {
   pressed: "pressed",
 };
+
+const synth = new Synth();
 
 const keyboard = document.getElementById("keyboard");
 const keys = Array.from(keyboard.children);
@@ -8,10 +12,14 @@ const keys = Array.from(keyboard.children);
 function toggleKey(key, flag) {
   if (flag) {
     keys[key].classList.add(cl.pressed);
+    synth.pressKey(key);
   } else {
     keys[key].classList.remove(cl.pressed);
+    synth.releaseKey(key);
   }
 }
+
+// MOUSE CONTROL
 
 keyboard.addEventListener("pointerdown", (e) => {
   const key = keys.indexOf(e.target.closest(".key"));
@@ -19,11 +27,14 @@ keyboard.addEventListener("pointerdown", (e) => {
 
   function onPointerUp() {
     toggleKey(key, false);
+
     window.removeEventListener("pointerup", onPointerUp);
   }
 
   window.addEventListener("pointerup", onPointerUp);
 });
+
+// KEYBOARD CONTROL
 
 const noteKeys = {
   q: 0,
@@ -60,11 +71,10 @@ function isNoteKey(key) {
 }
 
 window.addEventListener("keydown", (e) => {
-  if (!isNoteKey(e.key)) return;
+  if (!isNoteKey(e.key) || e.repeat) return;
 
   const keyDown = firstKey + noteKeys[e.key];
 
-  if (keys[keyDown].classList.contains(cl.pressed)) return;
   toggleKey(keyDown, true);
 
   function onKeyUp(e) {
@@ -79,4 +89,171 @@ window.addEventListener("keydown", (e) => {
   }
 
   window.addEventListener("keyup", onKeyUp);
+});
+
+// TRACKER
+
+const exampleTrack = [
+  // C E G B
+  { time: 0, key: 39, duration: 1000 },
+  { time: 0, key: 43, duration: 1000 },
+  { time: 0, key: 46, duration: 1000 },
+  { time: 0, key: 50, duration: 1000 },
+
+  // D F A C
+  { time: 1000, key: 41, duration: 1000 },
+  { time: 1000, key: 44, duration: 1000 },
+  { time: 1000, key: 48, duration: 1000 },
+  { time: 1000, key: 51, duration: 1000 },
+
+  // E G B D
+  { time: 2000, key: 43, duration: 1000 },
+  { time: 2000, key: 46, duration: 1000 },
+  { time: 2000, key: 50, duration: 1000 },
+  { time: 2000, key: 53, duration: 1000 },
+
+  // F A C E
+  { time: 3000, key: 44, duration: 1000 },
+  { time: 3000, key: 48, duration: 1000 },
+  { time: 3000, key: 51, duration: 1000 },
+  { time: 3000, key: 55, duration: 1000 },
+
+  // G B D F
+  { time: 4000, key: 46, duration: 1000 },
+  { time: 4000, key: 50, duration: 1000 },
+  { time: 4000, key: 53, duration: 1000 },
+  { time: 4000, key: 56, duration: 1000 },
+
+  // A C E G
+  { time: 5000, key: 48, duration: 1000 },
+  { time: 5000, key: 51, duration: 1000 },
+  { time: 5000, key: 55, duration: 1000 },
+  { time: 5000, key: 58, duration: 1000 },
+
+  // B D F A
+  { time: 6000, key: 50, duration: 1000 },
+  { time: 6000, key: 53, duration: 1000 },
+  { time: 6000, key: 56, duration: 1000 },
+  { time: 6000, key: 60, duration: 1000 },
+
+  // C E G B
+  { time: 7000, key: 51, duration: 1000 },
+  { time: 7000, key: 55, duration: 1000 },
+  { time: 7000, key: 58, duration: 1000 },
+  { time: 7000, key: 62, duration: 1000 },
+];
+
+const scale = 128 / 1000;
+
+const tracker = document.getElementById("tracker");
+const lines = Array.from(tracker.children);
+
+function getLength(track) {
+  const last = [...track]
+    .sort((a, b) => b.time + b.duration - a.time + a.duration)
+    .shift();
+
+  return (last.time + last.duration) * scale;
+}
+
+function renderTrack(track) {
+  tracker.style.height = `calc(${getLength(track)}px + 100%)`;
+
+  lines.forEach((line) => {
+    line.textContent = "";
+  });
+
+  track.forEach(({ time, key, duration }) => {
+    const line = lines[key];
+
+    const event = document.createElement("div");
+    event.classList.add("event");
+
+    event.style.bottom = time * scale + "px";
+    event.style.height = duration * scale + "px";
+
+    line.append(event);
+  });
+}
+
+renderTrack(exampleTrack);
+
+// PLAYBACK
+
+function getPlayingKeys(track, time) {
+  const playing = track.filter(
+    (event) => event.time <= time && time <= event.time + event.duration
+  );
+
+  return playing.map((event) => event.key);
+}
+
+function play(track) {
+  let id;
+  let lastTime;
+  let startTime;
+
+  const length = getLength(track);
+  let scrolled = 0;
+
+  function frame(time) {
+    if (typeof lastTime === "undefined") {
+      lastTime = time;
+      startTime = time;
+    }
+
+    const delta = time - lastTime;
+    const elapsed = time - startTime;
+
+    lastTime = time;
+    scrolled += delta * scale;
+
+    tracker.style.transform = `translate3d(0, ${scrolled}px, 0)`;
+
+    const playing = getPlayingKeys(track, elapsed);
+
+    synth.getPlaying().forEach((key) => {
+      if (!playing.includes(key)) {
+        toggleKey(key, false);
+      }
+    });
+
+    playing.forEach((key) => toggleKey(key, true));
+
+    if (scrolled <= length) {
+      id = requestAnimationFrame(frame);
+    } else {
+      stop();
+    }
+  }
+
+  id = requestAnimationFrame(frame);
+
+  return function stop() {
+    tracker.style.transform = null;
+    keys.forEach((_, key) => toggleKey(key, false));
+    synth.stop();
+    cancelAnimationFrame(id);
+  };
+}
+
+// CONTROLS
+
+let stop = null;
+
+const controls = {
+  play: document.getElementById("play"),
+  stop: document.getElementById("stop"),
+  record: document.getElementById("record"),
+};
+
+controls.play.addEventListener("click", () => {
+  stop = play(exampleTrack);
+});
+
+controls.stop.addEventListener("click", () => {
+  if (stop !== null) {
+    stop();
+    stop = null;
+  }
 });
---
+++ synth.js
@@ -0,0 +1,93 @@
+const F0 = 27.5;
+const GAIN_DELAY = 0.01;
+
+function keyToFreq(key) {
+  const fn = F0 * 2 ** (key / 12);
+  return Math.round(fn * 100) / 100;
+}
+
+class Synth {
+  playing = {};
+  ctx = new AudioContext();
+
+  startOscillator = (key) => {
+    if (this.playing[key]) return;
+
+    const osc = this.ctx.createOscillator();
+    const vol = this.ctx.createGain();
+
+    this.playing[key] = osc;
+
+    osc.vol = vol;
+    osc.type = "sine";
+    osc.frequency.value = keyToFreq(key);
+    vol.gain.value = 0;
+
+    osc.connect(vol);
+    vol.connect(this.ctx.destination);
+
+    osc.start();
+  };
+
+  stopOscillator = (key) => {
+    if (!this.playing[key]) return;
+
+    const osc = this.playing[key];
+    const end = osc.context.currentTime + GAIN_DELAY;
+
+    delete this.playing[key];
+
+    osc.vol.gain.linearRampToValueAtTime(0, end);
+    osc.stop(end);
+  };
+
+  balanceOscillators = () => {
+    const gain = 0.95 / Object.keys(this.playing).length;
+
+    for (const key in this.playing) {
+      const osc = this.playing[key];
+      const end = osc.context.currentTime + GAIN_DELAY;
+      osc.vol.gain.linearRampToValueAtTime(gain, end);
+    }
+  };
+
+  getPlaying = () => {
+    return Object.keys(this.playing).map((key) => parseInt(key, 10));
+  };
+
+  pressKey = (key) => {
+    this.startOscillator(key);
+    this.balanceOscillators();
+  };
+
+  releaseKey = (key) => {
+    this.stopOscillator(key);
+    this.balanceOscillators();
+  };
+
+  play = (keys) => {
+    const prevKeys = this.getPlaying();
+
+    prevKeys.forEach((key) => {
+      if (!keys.includes(key)) {
+        this.startOscillator(key);
+      }
+    });
+
+    keys.forEach((key) => {
+      if (!prevKeys.includes(key)) {
+        this.stopOscillator(key);
+      }
+    });
+
+    this.balanceOscillators();
+  };
+
+  stop = () => {
+    this.getPlaying().forEach((key) => {
+      this.stopOscillator(key);
+    });
+  };
+}
+
+export default Synth;
